2b766c8c711bd227c3e508ab07c31447dc3882ea
Yosuke Ota <otameshiyo23@gmail.com>
2021-10-16 17:22:58 +0900

    Add tests (#8)

---
+++ test/backward-compat.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const expect = require("chai").expect;
+const syntax = require("../");
+
+describe("backward compatible tests", () => {
+	it("Invalid HTML", () => {
+		const html = `
+		<style lang="custom">
+		// comment
+		</style>`;
+		const document = syntax({
+			rules: [
+				{
+					test: /\.custom$/,
+					lang: "scss",
+				},
+			],
+		}).parse(html, {
+			from: "foo.html",
+		});
+		expect(document.source).to.haveOwnProperty("lang", "html");
+		expect(document.nodes).to.have.lengthOf(1);
+		expect(document.nodes[0].type).equal("root");
+		expect(document.nodes[0].nodes).to.have.lengthOf(1);
+		expect(document.nodes[0].nodes[0].type).equal("comment");
+	});
+});
