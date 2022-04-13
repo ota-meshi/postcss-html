02d7b592749a9a06bfb81a0871e4cc6a8e63c816
yosuke ota <otameshiyo23@gmail.com>
2022-04-13 10:13:57 +0900

    Fix incorrect end loc for style attr template

---
+++ test-fixtures/ast/test.svelte
@@ -0,0 +1,6 @@
+<script>
+	let bgOpacity = 0.5;
+	$: color = bgOpacity < 0.6 ? '#000' : '#fff';
+</script>
+<p style="color: {color}; --opacity: {bgOpacity};">This is a paragraph.</p>
+<p style="color: {color}">This is a paragraph.</p>
