b9f04047344ce9118ec6fae5f236badd914d1848
Yosuke Ota <otameshiyo23@gmail.com>
2022-04-15 14:53:54 +0900

    Handle interpolation for svelte (#45)
    * Handle interpolation for svelte
    
    * refactor

---
+++ test-fixtures/ast/brace.svelte
@@ -0,0 +1,6 @@
+<script>
+	let color = '#f00';
+	let value
+</script>
+<input class:over-100={value > 100} bind:value={value} style="color: {color}">
+<input class:over-100={  ((value > 100) )  } bind:value={value} style="color: {color}">
