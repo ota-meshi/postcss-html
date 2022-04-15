e85396b8d0ae5dfc1fddecd2b64993fbe2c7b0f1
yosuke ota <otameshiyo23@gmail.com>
2022-04-15 14:47:55 +0900

    refactor

--- test-fixtures/ast/brace.svelte
+++ test-fixtures/ast/brace.svelte
@@ -3,3 +3,4 @@
 	let value
 </script>
 <input class:over-100={value > 100} bind:value={value} style="color: {color}">
+<input class:over-100={  ((value > 100) )  } bind:value={value} style="color: {color}">
