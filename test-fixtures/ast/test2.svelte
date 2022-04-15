3f90622bc970744c31d95f8e5e5bfd067918a530
Yosuke Ota <otameshiyo23@gmail.com>
2022-04-15 16:09:13 +0900

    Fix syntax error in complex interpolation on Svelte (#46)

---
+++ test-fixtures/ast/test2.svelte
@@ -0,0 +1,6 @@
+<script>
+	let bgOpacity = 0.5;
+	$: color = bgOpacity < 0.6 ? '#000' : '#fff';
+	$: styleOpacity = `--opacity: ${bgOpacity};`
+</script>
+<p style="color: {color}; {styleOpacity}">This is a paragraph.</p>
