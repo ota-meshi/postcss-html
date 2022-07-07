440e0e8eebdc1b41569536e3c4c3f2a1a824fd33
Yosuke Ota <otameshiyo23@gmail.com>
2022-07-07 14:43:05 +0900

    Fixed incorrect parsing of svelte. (#61)

--- test-fixtures/ast/brace.svelte
+++ test-fixtures/ast/brace.svelte
@@ -4,3 +4,10 @@
 </script>
 <input class:over-100={value > 100} bind:value={value} style="color: {color}">
 <input class:over-100={  ((value > 100) )  } bind:value={value} style="color: {color}">
+
+
+<style>
+input {
+	color: red;
+}
+</style>
