afa8f9264051390581e18007389a26a2fcc8b4aa
yosuke ota <otameshiyo23@gmail.com>
2022-07-07 15:49:29 +0900

    Add test case

--- test-fixtures/ast/brace.svelte
+++ test-fixtures/ast/brace.svelte
@@ -1,10 +1,14 @@
 <script>
-	let color = '#f00';
+	let color1 = '#f00';
+	let color2 = '#0f0';
+	let color3 = '#00f';
+	let color4 = '#ff0';
 	let value
 </script>
-<input class:over-100={value > 100} bind:value={value} style="color: {color}">
-<input class:over-100={  ((value > 100) )  } bind:value={value} style="color: {color}">
-
+<input class:over-100={value > 100} bind:value={value} style="color: {color1}">
+<input class:over-100={  ((value > 100) )  } bind:value={value} style="color: {color2}">
+<input class={ `${(value > 100) ? 'foo' : 'bar'}`  } style="color: {color3}">
+<input class={ {foo: (value > 100) ? 'foo' : 'bar'}.foo  } style="color: {color4}">
 
 <style>
 input {
